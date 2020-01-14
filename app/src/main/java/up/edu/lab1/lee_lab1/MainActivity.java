26499d3720a7e37f914ea5b58146f7a78ce8055e
Jaskatone <jaskatone@gmail.com>
2020-01-13 20:29:05 -0800

    initial commit

---
+++ app/src/main/java/up/edu/lab1/lee_lab1/MainActivity.java
@@ -0,0 +1,14 @@
+package up.edu.lab1.lee_lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
